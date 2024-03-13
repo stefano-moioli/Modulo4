1c8e5a46dc40fc16337f002d881d30b206631b14
stefano-moioli <stefano.moioli10@gmail.com>
2024-03-13 16:34:11 +0100

    callFetch

---
+++ W1D4/js/script.js
@@ -0,0 +1,29 @@
+/* const getAlbums = async ()=>{
+    try {
+        const response = await fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=eminem")
+        const data = await response.json();
+        console.log(data);
+        return data
+    } catch (error) {
+        console.log("Errore durante la richiesta degli album");
+    }
+}*/
+
+fetch('https://striveschool-api.herokuapp.com/api/deezer/search?q=eminem')
+  .then(response => {
+    // Controlla se la risposta è OK (status 200-299)
+    if (!response.ok) {
+      throw new Error(`Errore di rete: ${response.status}`);
+    }
+    // Parsifica la risposta JSON
+    return response.json();
+  })
+  .then(data => {
+    // Fai qualcosa con i dati ottenuti
+    console.log(data);
+  })
+  .catch(error => {
+    // Gestisci gli errori di rete o di parsing
+    console.error('Si è verificato un errore:', error);
+  });
+
